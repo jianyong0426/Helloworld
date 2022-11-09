c725a6e2b3c34306bf381db323da8cd47aac72b9
jianyong0426 <jianyong0426@gmail.com>
2022-11-09 17:21:12 +0800

    first commit

---
+++ LoadBalancerTesting/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoadBalancerTesting.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Console.WriteLine("Hello World");
+            return Ok("Hello World");
+        }
+    }
+}
